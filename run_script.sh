df4e2a74ec1a6bdb47862be257825a38c2caeeb1
JunyongPark97 <pjyong68@naver.com>
2021-01-08 16:13:04 +0900

    [ADDED] run script for using Django Server

---
+++ run_script.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+source ~/.profile
+/home/human/anaconda3/envs/human-parser/bin/python simple_extractor.py --dataset 'lip' --model-restore 'checkpoints/exp-schp-201908261155-lip.pth' --input-dir 'inputs' --output-dir 'outputs'
